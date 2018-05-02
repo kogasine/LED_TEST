ec39d0b3071a0ee8bc3a5f1b996ca0448b14eff1
DESKTOP-TSHV0O5\1234 <id-_-bi@hanmail.net>
2018-05-02 15:42:05 +0900

    300ms blink

---
+++ 180502_LED/180502_LED/main.cpp
@@ -0,0 +1,19 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRD=0xff;
+	PORTD=0xff;
+    /* Replace with your application code */
+    while (1) 
+    {
+		PORTD = 0x00;
+		_delay_ms(300);
+		PORTD = 0xff;
+		_delay_ms(300);
+    }
+}
+
